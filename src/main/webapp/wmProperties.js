0ef290ab156617549cf51646967528a497974b75
ZMlWxmArSN <automate1@wavemaker.com>
2017-11-14 17:46:38 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_TGyQHtoqix",
+  "homePage" : "Main",
+  "name" : "Auto_TGyQHtoqix",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
